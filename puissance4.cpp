2a0fd2a8dd98df299da80e5ce3450193a3808907
totoletoto69 <thomas.boyer69100@gmail.com>
2023-02-10 23:53:33 +0100

    programme qui marche mais nul

--- puissance4.cpp
+++ puissance4.cpp
@@ -2,6 +2,31 @@
 #include <vector>
 using namespace std;
 
+int reward(vector<vector<int>> & board, int y){
+	return y;
+}
+int strategy(vector<vector<int>> & board){
+	int maxPoints = reward(board, 0);
+	int y = 0;
+	int points;
+	for(int i = 1; i < board[0].size(); ++i){
+		points = reward(board, i);
+		if(points>maxPoints){
+			maxPoints = points;
+			y = i;
+		}
+	}	
+	return y;
+}
+
+int fallHeight(vector<vector<int>> & board, int y){
+	int x = board.size();
+	while(x > 0 && board[x-1][y] == 0){
+		x -= 1;
+	}
+	return x;
+}
+
 int main(){
 	//Récupération des valeurs du cin et création du plateau
 	int W,H,N,S,i;
@@ -11,4 +36,20 @@ int main(){
 		vector<int> myRow(W, 0);
 		board.push_back(myRow);
 	}
+
+	//Gestion du jeu
+	int player = 0;
+	int x, y;
+
+	while(1){
+		player = player % N + 1;
+		if(player == S){
+			y = strategy(board); 
+			cout << y << endl;
+		} else {
+			cin >> y;
+		}
+		y = fallHeight(board, y);
+		board[x][y] = player;
+	}	
 }
